107bd79f9bc6bf5f169721a30f0f2dcc04e13056
Davide Bizzarri <davide@plutodigital.com>
2022-05-10 18:13:40 +0200

    chore: add WETH and WBTC dev funds

--- tasks/fork/accounts.ts
+++ tasks/fork/accounts.ts
@@ -9,7 +9,14 @@ const BINANCE14_ADDRESS = "0xE78388b4CE79068e89Bf8aA7f218eF6b9AB0e9d0";
 // Account with large sums of YOP: https://etherscan.io/token/0xae1eaae3f627aaca434127644371b67b18444051?a=0x0a5f46c51076e9cefe146417725ceec2e448c3ef
 const YOP_RESERVE_ADDRESS = "0x0a5f46c51076e9cefe146417725ceec2e448c3ef";
 
+// Account with large sums of:
+// * WETH: https://etherscan.io/token/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2?a=0xe78388b4ce79068e89bf8aa7f218ef6b9ab0e9d0
+// * WBTC: https://etherscan.io/token/0x2260fac5e5542a773aa44fbcfedf7c193bc2c599?a=0xe78388b4ce79068e89bf8aa7f218ef6b9ab0e9d0
+const AVALANCHE_BRIDGE_ADDRESS = "0xe78388b4ce79068e89bf8aa7f218ef6b9ab0e9d0";
+
 export const ETH_ADDRESS = BINANCE7_ADDRESS;
+export const WETH_ADDRESS = AVALANCHE_BRIDGE_ADDRESS;
+export const WBTC_ADDRESS = AVALANCHE_BRIDGE_ADDRESS;
 export const USDC_ADDRESS = BINANCE14_ADDRESS;
 export const DAI_ADDRESS = BINANCE14_ADDRESS;
 export const YOP_ADDRESS = YOP_RESERVE_ADDRESS;
